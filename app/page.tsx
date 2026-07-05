fbfc37b2c83ac0a1798d689b94a2c0362b314a53
Sumayea Rahman <shorbonam104@gmail.com>
2026-07-06 00:18:28 +0600

    page.tsx er about typo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { Navbar } from "@/components/layout/Navbar"
 import { Footer } from "@/components/layout/Footer"
 import { Hero } from "@/components/sections/Hero"
-import { About } from "@/components/sections/about"
+import { About } from "@/components/sections/About"
 import { Skills } from "@/components/sections/Skills"
 import { Projects } from "@/components/sections/Projects"
 import { Education } from "@/components/sections/Education"
